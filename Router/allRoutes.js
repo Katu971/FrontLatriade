7f0643ea385d0b62b18ea67fd14db43b34c1e44a
Your Name <you@example.com>
2025-05-24 19:29:38 +0200

    ajout page Emmanuelle.html

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Katucya", "GalerieKatucya", "/pages/Katucya.html"),];
-
+    new Route("/GalerieKatucya", "Katucya", "/pages/Katucya.html"),
+    new Route("/GalerieEmmanuelle", "Emmanuelle", "/pages/Emmanuelle.html"),];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "La triade";
