33a56783233ec310efa010aa43be1382dd3bdbe0
Your Name <you@example.com>
2025-05-29 16:42:29 +0200

    page contact.html créé

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/GalerieKatucya", "Katucya", "/pages/Katucya.html"),
-    new Route("/GalerieEmmanuelle", "Emmanuelle", "/pages/Emmanuelle.html"),];
+    new Route("/GalerieEmmanuelle", "Emmanuelle", "/pages/Emmanuelle.html"),
+    new Route("/GalerieSarah", "Sarah", "/pages/Sarah.html"),
+    new Route("/Contact", "Contact La Triade", "/pages/Contact.html"),];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "La triade";
