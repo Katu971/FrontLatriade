5da7b26767407028200d4e64f57460dc6cf0c718
Your Name <you@example.com>
2025-06-03 20:18:16 +0200

    ajout pages contact tatoueuses

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,10 @@ export const allRoutes = [
     new Route("/GalerieKatucya", "Katucya", "/pages/Katucya.html"),
     new Route("/GalerieEmmanuelle", "Emmanuelle", "/pages/Emmanuelle.html"),
     new Route("/GalerieSarah", "Sarah", "/pages/Sarah.html"),
-    new Route("/Contact", "Contact La Triade", "/pages/Contact.html"),];
+    new Route("/Contact", "Contact La Triade", "/pages/Contact.html"),
+    new Route("/ContactKatucya", "Contact Katucya", "/pages/ContactKatucya.html"),
+    new Route("/ContactEmmanuelle", "Contact Emmanuelle", "/pages/ContactEmmanuelle.html"),
+    new Route("/ContactSarah", "Contact Sarah", "/pages/ContactSarah.html"),
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "La triade";
